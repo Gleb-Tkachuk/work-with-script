542af097e1bfc547577c0a688ab4a78bfdd9a249
Gleb-Tkachuk <150186145+Gleb-Tkachuk@users.noreply.github.com>
2025-09-13 12:16:32 +0200

    first commit

---
+++ domashka_script.js
@@ -0,0 +1,9 @@
+let day = getCurrentDay();
+console.log(day);
+let month = getCurrentMonth();
+console.log(month);
+let year = getCurrentYear();
+console.log(year);
+
+let date = getDate("2025/09/10 16:43:10");
+console.log(date);
