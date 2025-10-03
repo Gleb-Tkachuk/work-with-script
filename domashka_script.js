473876c4c7359070bc342b1199fe9c674bc03ea8
Gleb-Tkachuk <150186145+Gleb-Tkachuk@users.noreply.github.com>
2025-10-03 12:02:53 +0200

    third commit

--- domashka_script.js
+++
@@ -1,9 +0,0 @@
-let day = getCurrentDay();
-console.log(day);
-let month = getCurrentMonth();
-console.log(month);
-let year = getCurrentYear();
-console.log(year);
-
-let date = getDate("2025/09/10 16:43:10");
-console.log(date);
